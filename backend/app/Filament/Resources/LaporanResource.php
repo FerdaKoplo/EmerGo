22c85c46fcca798077f02fc1830b81872ac26f2c
Farid | Iamjustzero <imjustzero@duck.com>
2025-07-29 18:24:53 +0700

    add laporan resource view

---
+++ backend/app/Filament/Resources/LaporanResource.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LaporanResource\Pages;
+use App\Filament\Resources\LaporanResource\RelationManagers;
+use App\Models\Laporan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LaporanResource extends Resource
+{
+    protected static ?string $model = Laporan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nomor')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('nama_insiden')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DateTimePicker::make('tanggal_waktu')
+                    ->required(),
+                Forms\Components\TextInput::make('lokasi')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('status')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('waiting'),
+                Forms\Components\TextInput::make('kategori')
+                    ->required()
+                    ->maxLength(255)
+                    ->default('damkar'),
+                Forms\Components\Textarea::make('deskripsi')
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('foto')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('nomor')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('nama_insiden')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('tanggal_waktu')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('lokasi')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('kategori')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('foto')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLaporans::route('/'),
+            'create' => Pages\CreateLaporan::route('/create'),
+            'edit' => Pages\EditLaporan::route('/{record}/edit'),
+        ];
+    }
+}

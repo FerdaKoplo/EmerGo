e5a7f4da3b432f124c0c92b074b5d9764351cf03
Farid | Iamjustzero <imjustzero@duck.com>
2025-07-29 15:35:38 +0700

    make model Statistik -a

---
+++ backend/app/Http/Controllers/StatistikController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreStatistikRequest;
+use App\Http\Requests\UpdateStatistikRequest;
+use App\Models\Statistik;
+
+class StatistikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStatistikRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Statistik $statistik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Statistik $statistik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStatistikRequest $request, Statistik $statistik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Statistik $statistik)
+    {
+        //
+    }
+}

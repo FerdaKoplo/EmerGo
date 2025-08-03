a8c2f2418c6b206cdc50b940ed3f971f4395197d
Farid | Iamjustzero <imjustzero@duck.com>
2025-08-03 21:51:57 +0700

    fix: update title and add favicon to welcome page

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
-    <title>Test</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
+    <title>EmerGo</title>
 </head>
 
 <body class="font-sans antialiased bg-white text-gray-800">
